7ab794cab8de2ff224baa907368ce6e6a27ae151
Nony <nonyo@mit.edu>
2025-04-12 18:26:36 -0400

    prog on next step

--- index.js
+++ index.js
@@ -36,18 +36,30 @@ createApp({
       for (const { object } of messageObjectsIterator) {
         console.log("this is the message abt to be pushed", object.value.content);
         if (object.value.content == "") {
-          console.log("bad message, skipping")
+          console.log("bad message, skipping");
         }
         else {
-          console.log("good message (not empty)");
+          console.log("good message (not empty)", object.value.published);
           newMessageObjects.push(object);
         }
 
       }
 
       // Sort here
+      toSorted(first_came_first())
 
       this.messageObjects = newMessageObjects;
+
+      function first_came_first(message_1, message_2) {
+        let ret = 0;
+        // message 1 is older than message 2
+        if (message_1.value.published > message_2.value.published) {
+          ret = -1
+        }
+      }
+
+
+
     },
 
     *getMessageObjectsIterator() {
